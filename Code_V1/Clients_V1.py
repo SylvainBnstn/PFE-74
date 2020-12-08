2d9f888a0bec255afebdce33fd22738ba68b0fdc
SylvainBnstn <36404524+SylvainBnstn@users.noreply.github.com>
2020-12-08 13:30:45 +0100

    Creation of .py

---
+++ Code_V1/Clients_V1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 13:30:18 2020
+
+@author: Sylvain
+"""
+
+
---
+++ Code_V1/Market_V1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 13:29:46 2020
+
+@author: Sylvain
+"""
+
+
