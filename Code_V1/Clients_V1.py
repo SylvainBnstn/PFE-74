6b5659c00d177c06f260228434659a58e3c56ce6
SylvainBnstn <36404524+SylvainBnstn@users.noreply.github.com>
2020-12-09 17:22:32 +0100

    Ajout de la class list client

--- Code_V1/Clients_V1.py
+++ Code_V1/Clients_V1.py
@@ -4,7 +4,7 @@ Created on Tue Dec  8 13:30:18 2020
 
 @author: Sylvain
 """
-
+import random
 
 class Naiv_client:
     
@@ -23,8 +23,25 @@ class Naiv_client:
         r=(self.prix_min,self.prix_max,self.will_to_pay)
         return str(r)
     
+class list_naiv_clients:
+    
+    def __init__(self,prix_min,prix_max,nb_client):
+        self.clients_list=[]
+        for _ in range(nb_client):
+            temp_min = random.uniform(prix_min,prix_min+(prix_max-prix_min)/2)
+            temp_max = random.uniform(prix_min+(prix_max-prix_min)/2,prix_max)
+            temp_wtp = random.random()
+            self.clients_list.append(Naiv_client(temp_min,temp_max,temp_wtp))
+    
+    def __str__(self):
+        return str(self.clients_list)
 
+    
 a=Naiv_client(10,15,0.2)
 a2=Naiv_client(12,15,0.7)
 list2=[a,a2]
 print(list2)
+list1=list_naiv_clients(10,20,10)
+print(list1)
+
+
