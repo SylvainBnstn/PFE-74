b17f9caf99f2a82b211082dec2269007d74daf8b
Panathan <jonathan.panathan@hotmail.fr>
2021-01-14 16:33:26 +0100

    Commit

---
+++ Code_V1/Market_V1.py
@@ -0,0 +1,113 @@
+ # -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 13:29:46 2020
+
+@author: Sylvain
+"""
+
+import Naiv_Clients_v2 as cnt
+import data_analysis as da
+import airbnb_processing as ap
+import Test_Class as tst
+
+
+import random as rd
+import pandas as pd
+
+# pour rappel 
+# Class list_naiv_clients est l'objet qui contient tous les clients
+# on l'appel donc naiv_clients
+# Cette objet contient la list des objets clients qui contiennent eux-mêmes leurs prix min max et WTP
+
+class Market:
+    
+    def __init__(self,clients):
+        self.clients = clients
+        
+        data=[0,0,0,0,0,0,0,0]
+        self.df_naiv_sales = pd.DataFrame(columns= ["Ite","Prix","Achat_Envi","Achat_Real","Achat_Aban","Achat_Inst","Achat_Repou","Achat_Tot"])
+        self.df_naiv_sales.loc[0]=data
+
+        
+    #fonction d'execution des ventes
+    def check_sales(self, price, clients, list_resa):
+        
+        list_sales, temp_envi, temp_real, temp_aban, temp_inst, temp_repou, list_resa = clients.check_sales_naiv(price, list_resa)
+        
+        nb_row=self.df_naiv_sales.shape[0]
+        data_temp=[nb_row,price,temp_envi,temp_real,temp_aban,temp_inst,temp_repou,temp_inst+temp_real]
+        self.df_naiv_sales.loc[nb_row]=data_temp
+        return list_sales, list_resa
+        
+
+
+def test():
+    
+    ###################################
+    
+    df_start=ap.load_data("airbnb_data.csv")
+    df_start=df_start.loc[(df_start["room_type"]=="Entire home/apt") & (df_start["price"]>=100) & (df_start["price"]<=200)]
+    df_final=da.review_data(df_start,"new-york-city")
+
+    df_final=df_final.reset_index(drop=True)
+    df_final=df_final.loc[df_final.shape[0]-12:df_final.shape[0]-1]
+    
+    #définit une list de clients naif avec prix min, prix max et nb clients
+    naiv_clients= cnt.list_naiv_clients(100,200,30,0.85,0.15)
+    
+    #on init le DQN
+    dqn=tst.DQN()
+    
+    #on créé le marché
+    market=Market(naiv_clients)
+    
+    #TEEEEEEEEST
+    
+    ########
+    state = dqn.env_initial_test_state(150)#init price 
+    reward_trace = []
+    p_trace = [state[0]]
+    #############
+    
+    
+    list_resa = [0]* df_final.shape[0]
+    list_resa_final = []    
+    
+    for k in range(df_final.shape[0]):    
+        
+        #arrivée d'un unique prix 
+        
+        ########## Remplacer par un aleat compris entre 50 et 250
+        reward,p, state= dqn.dqn_test(state)
+        reward_trace.append(reward)
+        p_trace.append(p)
+        
+        #########
+        
+        print(p)
+        
+        avail_rate=df_final.loc[df_final.index[k],"mean_availability_30"] / 30 
+        final_rate=avail_rate+0.05
+        
+        list_del, list_resa = market.check_sales(p,naiv_clients,list_resa)
+        naiv_clients.update_client(100, 200, list_del,0.85,final_rate,k)
+        
+        print (list_resa)
+        
+        list_resa_final.append(list_resa[0])
+        del list_resa[0]
+        
+        #retour de la demande 
+        
+    print(df_final)
+    print(list_resa_final)    
+    print(market.df_naiv_sales)
+    
+
+
+        
+
+        
+        
+test()        
+        
---
+++ Code_V1/Model_DQN_update.py
@@ -0,0 +1,171 @@
+import numpy as np
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+from collections import namedtuple
+import torch.nn.functional as F
+import pandas as pd
+
+import torch
+import torch.optim as optim
+
+from DQN_V2 import DeepQNetwork
+from Replay_memory import ReplayMemory
+from Eps_Greedy_Policy import EpsilonGreedyPolicy
+import Environment 
+
+# Visualization functions
+def plot_return_trace(returns, smoothing_window=10, range_std=2):
+  plt.figure(figsize=(16, 7))
+  plt.xlabel("Episode")
+  plt.ylabel("Return ($)")
+  returns_df = pd.Series(returns)
+  ma = returns_df.rolling(window=smoothing_window).mean()
+  mstd = returns_df.rolling(window=smoothing_window).std()
+  plt.plot(ma, c = 'blue', alpha = 1.00, linewidth = 1)
+  plt.fill_between(mstd.index, ma-range_std*mstd, ma+range_std*mstd, color='blue', alpha=0.2)
+
+def plot_price_schedules(p_trace, sampling_ratio, last_highlights,T, fig_number=None):
+  plt.figure(fig_number);
+  plt.xlabel("Time step");
+  plt.ylabel("Price ($)");
+  plt.plot(range(T), np.array(p_trace[0:-1:sampling_ratio]).T, c = 'k', alpha = 0.05)
+  return plt.plot(range(T), np.array(p_trace[-(last_highlights+1):-1]).T, c = 'red', alpha = 0.5, linewidth=2)
+
+
+###############################################################################
+# Update the model
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+GAMMA = 0.9
+TARGET_UPDATE = 20
+BATCH_SIZE = 512
+
+import testwithdata
+
+data_train = testwithdata.get_data()[1].to_numpy()
+#T = len(data_train)
+T = 12 # 12mois
+
+price_grid = Environment.price_grid
+profit_t_response = Environment.profit_t_response
+profit_response = Environment.profit_response
+state_dim = Environment.state_dim
+
+def update_model(memory, policy_net, target_net):
+    if BATCH_SIZE < len(memory):
+            
+        transitions = memory.sample(BATCH_SIZE)
+        batch = Transition(*zip(*transitions))
+    
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=device, dtype=torch.uint8)
+        non_final_next_states = torch.stack([s for s in batch.next_state if s is not None])
+        #non_final_next_states = torch.stack([s for s in batch.next_state if s is not None])
+        
+        state_batch = torch.stack(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.stack(batch.reward)
+    
+        #q-value
+        state_action_values = policy_net(state_batch).gather(1, action_batch)
+    
+        next_state_values = torch.zeros(BATCH_SIZE, device=device)
+        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+        #next_state_values = target_net(non_final_next_states).max(1)[0].detach()
+    
+        # Compute the expected Q values
+        expected_state_action_values = reward_batch[:, 0] + (GAMMA * next_state_values)  
+    
+        # Compute Huber loss
+        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+        
+        # Optimize the model
+        optimizer.zero_grad()
+        loss.backward()
+        for param in policy_net.parameters():
+            param.grad.data.clamp_(-1, 1)
+        optimizer.step()
+    
+def env_initial_state():
+    state = np.repeat(0,2*state_dim)
+    return state
+
+def env_step( state, action):
+    next_state = np.repeat(0,2*state_dim)
+    next_state[0] = price_grid[action]
+    next_state[1:state_dim] = state[0:state_dim-1]
+    reward = profit_t_response(next_state[0], next_state[1])
+    return next_state, reward
+
+def to_tensor(x):
+  return torch.from_numpy(np.array(x).astype(np.float32))
+
+def to_tensor_long(x):
+  return torch.tensor([[x]], device=device, dtype=torch.long)
+
+
+###############################################################################
+# Training
+policy_net = DeepQNetwork(2*state_dim, len(price_grid)).to(device)
+target_net = DeepQNetwork(2*state_dim, len(price_grid)).to(device)
+optimizer = optim.Adam(policy_net.parameters(), lr = 0.005)
+policy = EpsilonGreedyPolicy()
+memory = ReplayMemory(10000)
+
+target_net.load_state_dict(policy_net.state_dict())
+target_net.eval()
+
+def training():
+        
+    num_episodes = 100
+    return_trace = []
+    p_trace = [] # price schedules used in each episode
+    for i_episode in range(num_episodes):
+        state = env_initial_state()
+        reward_trace = []
+        p = []
+        for t in range(T):
+            # Select and perform an action
+            with torch.no_grad():
+              q_values = policy_net(to_tensor(state))
+            action = policy.select_action(q_values.detach().numpy())
+    
+            next_state, reward = env_step(state, action)
+    
+            # Store the transition in memory
+            memory.push(to_tensor(state), 
+                        to_tensor_long(action), 
+                        to_tensor(next_state) if t != state_dim - 1 else None, 
+                        to_tensor([reward]))
+    
+            # Move to the next state
+            state = next_state
+    
+            # Perform one step of the optimization (on the target network)
+            update_model(memory, policy_net, target_net)
+    
+            reward_trace.append(reward)
+            p.append(price_grid[action])
+    
+        return_trace.append(sum(reward_trace))
+        p_trace.append(p)
+    
+        # Update the target network, copying all weights and biases in DQN
+        if i_episode % TARGET_UPDATE == 0:
+            target_net.load_state_dict(policy_net.state_dict())
+    
+            clear_output(wait = True)
+            print(f'Episode {i_episode} of {num_episodes} ({i_episode/num_episodes*100:.2f}%)')
+    
+    plot_return_trace(return_trace)
+    fig = plt.figure(figsize=(16, 7))
+    plot_price_schedules(p_trace, 5, 1, T,fig.number)
+    
+    return return_trace, p_trace
+
+
+return_trace, p_trace = training()
+
+
+for profit in sorted(profit_response(s) for s in p_trace)[-10:]:
+    print(f'Best profit results: {profit}')
