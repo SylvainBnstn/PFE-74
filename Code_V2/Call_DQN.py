1dfeea2d948182a681b5d5685f49ca296d612487
Ciciboulette <60005002+Ciciboulette@users.noreply.github.com>
2021-02-04 21:39:26 +0100

    Add files via upload
    File with comment

--- Code_V2/Call_DQN.py
+++ Code_V2/Call_DQN.py
@@ -1,52 +1,25 @@
 from Model_DQN_VF import DQN
 
+### Initialise the DQN 
+
 dqn = DQN("Data_Model_2.csv",0.9,0.5,0.83,1/10,2/10)
 
-
+### executing the training part of the DQN 
 def execute_train():
+    ## setting 200 episodes for the trainig 
     return_trace, p_trace = dqn.dqn_training(200)
+    ### plot the result by using the plot_result function 
+    ### it shows the reward and the price for the training 
     dqn.plot_result(return_trace, p_trace)
     return return_trace, p_trace
-    
+
+
+### executing the test part 
 def execute_test():
-    #seq_reward_all_apt19, seq_price_all_apt19, seq_booked_all_apt19 = dqn.plot_result_test(dqn.data_test_2019, dqn.data_test_booked_2019, "2019")
-    #seq_reward_all_apt20, seq_price_all_apt20, seq_booked_all_apt20 = dqn.plot_result_test(dqn.data_test_2020, dqn.data_test_booked_2020, "2020")
-    
-    #dqn.dqn_test(dqn.price_grid_test)
-    #return seq_reward_all_apt19, seq_price_all_apt19, seq_booked_all_apt19, seq_reward_all_apt20, seq_price_all_apt20, seq_booked_all_apt20
-    seq_reward_all_apt, seq_price_all_apt, seq_booked_all_apt, reward_from_data = dqn.plot_result_test(dqn.price_grid_test)
-    return seq_reward_all_apt, seq_price_all_apt, seq_booked_all_apt, reward_from_data
+    ### plotting the result and return the sequences of reward, price and booked on the data 
+    seq_reward, seq_price, seq_booked, reward_from_data = dqn.plot_result_test(dqn.price_grid_test)
+    return seq_reward, seq_price, seq_booked, reward_from_data
 
+### Calling these 2 functions for the trainging and the testing of our model 
 rt , pt = execute_train()
-seq_reward_all_apt, seq_price_all_apt, seq_booked_all_apt, reward_from_data = execute_test()
-
-"""
-# this function replace the demand of the customer
-def get_booked(price):
-    import random
-    return random.randint(0,30)
-
-def execute_interaction():
-    state = dqn.env_initial_test_state([150,0])#init price 150 init demand 0
-    reward_trace = [0] #reward à 0 au début
-    p_trace = [state[0,0]]
-    booked =[state[0,1]]
-    
-    for t in range(10):
-        p, state= dqn.dqn_interaction(state)
-        
-        booking = get_booked(p)
-        state[1,0] = booking
-        reward = dqn.profit_t_d(state[0,0], state[1,0])
-
-        reward_trace.append(reward)
-        p_trace.append(p)
-        booked.append(booking)
-        
-    print("PRICE: ", p_trace)
-    print("REWARD: ", reward_trace)
-    print("BOOKED: ", booked)
-    return p_trace, reward_trace
-"""
-#seq_reward_all_apt19, seq_price_all_apt19, seq_booked_all_apt19, seq_reward_all_apt20, seq_price_all_apt20, seq_booked_all_apt20 = execute_test()
-#execute_interaction()
+seq_reward, seq_price, seq_booked, reward_from_data = execute_test()
--- Code_V2/Import_data_VF.py
+++ Code_V2/Import_data_VF.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 import Market_V1 as mkt
 import random as rd
@@ -7,21 +6,22 @@ import random as rd
 import data_analysis as da
 import airbnb_processing as ap
 
-
+# Get the train_proportion corresponding to a column index (= train_proportion * columns total)
 def get_proportion(b,proportion):
-    
     return int(b*proportion)
 
-
+# Clean the data
 def get_data(train_proportion,file_name = "airbnb_data.csv"):
     # Read the csv file
     result = pd.read_csv(file_name) 
-    #print(result)
+    
     result.duplicated().sum()
     result.drop_duplicates(inplace= True)
+    
+    # Number of unique date
     b=len(list(result.date.unique()))
     
-    # Get apt id on Entire apt and those which have a price over 69 months ~ 6 years (2015 to 2020)
+    # Get apt id on Entire apt and those which have a price on all the date in the data ~ 6 years (2015 to 2020)
     nb_id = result[result["room_type"] == "Entire home/apt"].groupby(["id"], as_index = False).size().reset_index(name="size")
     nb_id.sort_values(by=["size","id"], ascending= False, inplace= True)
     nb_id = nb_id.loc[nb_id["size"] >=b]
@@ -33,63 +33,56 @@ def get_data(train_proportion,file_name = "airbnb_data.csv"):
     
     # Each row is an id, and each column is a date
     data_with_all_date = df.pivot(index="id", columns="date", values = "price")
-    #print(data_with_all_date)
     booked_with_all_date = df.pivot(index="id", columns="date", values = "booked")
-    
     
     # Compute the mean price of each apt over time and keep apt with a mean price between 70 and 230
     mean_price_all_date = df.groupby("id", as_index = False).agg({"price" : ["mean"]})
     mean_price_all_date.columns = ["id","mean_price"]
     mean_price_all_date = mean_price_all_date[(mean_price_all_date["mean_price"] >= 70) & (mean_price_all_date["mean_price"]<=230)]
     
+    # Filter on unique id which have mean price between 70 and 230
     list_id = (mean_price_all_date["id"]).unique()
     data_with_all_date = data_with_all_date.loc[data_with_all_date.index.isin(list_id)]
-    
     booked_with_all_date=booked_with_all_date.loc[booked_with_all_date.index.isin(list_id)]
     
+    # train_proportion corresponding to a column index (= train_proportion * columns total)
     proportion = get_proportion(b,train_proportion)
     
-    return data_with_all_date, booked_with_all_date,proportion
+    return data_with_all_date, booked_with_all_date, proportion
 
 #df_p, df_b,proportion = get_data(0.83)
 
     
-    
 def training_data(df_price, df_booked,proportion):
-    # Get train data from 2015 to 2019
+    # Get train data for us we choose from 2015 to 2019
     data_train = df_price[df_price.columns[0:proportion]]
     
     price_grid_total=[]
     # Read through the k apt
     for k in range(len(data_train)):
         price = list(data_train.iloc[k])
-        #diff_prix=[False]
         demand=[0]
         date=[0]
         
-        # Compute for each apt, the variation of price and collect the demand (booked) in the data
+        # Compute for each apt, the demand (booked) in the data
         for i in range(1,len(data_train.iloc[k])):
-            #diff_prix.append( data_train.iloc[k][i] - data_train.iloc[k][i-1] )
             demand.append(df_booked.iloc[k][i])
-            ### date
+            # Date
             date.append(i)
-        # Concatenate the columns (price, variation_price, demand) for each apt
-        #price_grid_temp = np.c_[price,demand]
         
         price_grid_total.append(np.c_[price,demand,date])
         
-    #print(len(data_train.iloc[k]), "b")
     # Concatenate the price grid of all apt and get the unique in row
-    all_price_grid = np.concatenate(price_grid_total)
-    # all_unique_price_grid = np.unique(all_price_grid, axis=0)
-    #print(all_price_grid)
-    
+    all_price_grid = np.concatenate(price_grid_total)    
     return all_price_grid
     
 #df = training_data(df_p, df_b)
     
 def create_data(nb_mois_test):
+    # Load data from the file airbnb_processing
     df_start=ap.load_data("airbnb_data.csv")
+    
+    # Filter on "Entire home/apt" and where price is between 100 and 200
     df_start=df_start.loc[(df_start["room_type"]=="Entire home/apt") & (df_start["price"]>=100) & (df_start["price"]<=200)]
     df_final=da.review_data(df_start,"new-york-city")
     
@@ -98,21 +91,21 @@ def create_data(nb_mois_test):
     df_final=df_final.reset_index(drop=True)
     df_final=df_final.loc[df_final.shape[0]-nombre_de_mois_test:df_final.shape[0]-1]
     
+    # Creat the global file of data that we want, price, demand, data and strategic client proportion
     df_glob_sales = pd.DataFrame(columns= ["Prix","Achat_Tot","Date","Part_Strat"])
     
-        
-    
-    #parcours de prix 
+    # Loop on price
     for n in range (160):
         
         print(n, "% effectués")
         
-        #parcours de proportion
+        # Loop on proportion
         for i in range(100):
             
-            #on créé le marché
+            # The market is created
             market=mkt.Market(100,200,30,1-(i/100),0.85,0.15,df_start,df_final)
             
+            # Initialize the price
             p_trace=[rd.randrange(70,230)]
             
             #parcours de date
@@ -122,18 +115,17 @@ def create_data(nb_mois_test):
         
                 p_trace.append(p)
                 
-
-                
                 achat_naiv, achat_strat = market.updates(p,p_trace,k)
                 
-                
+                # Save data in the empty dataframe
                 df_glob_sales = df_glob_sales.append({"Prix":p,"Achat_Tot":(achat_naiv+achat_strat),"Date":k+1,"Part_Strat":(i/100)},ignore_index=True)
-                    
                     
     print(df_glob_sales)
     
+    # Save into a csv file
     df_glob_sales.to_csv("Data_Model.csv",index=False)
     
+"""
 def create_data_2(nb_mois_test):
     df_start=ap.load_data("airbnb_data.csv")
     df_start=df_start.loc[(df_start["room_type"]=="Entire home/apt") & (df_start["price"]>=100) & (df_start["price"]<=200)]
@@ -188,68 +180,31 @@ def create_data_2(nb_mois_test):
                 
                 p_trace.append(p)
                 
-
-                
                 achat_naiv, achat_strat = market.updates(p,p_trace,k)
                 
-                
                 df_glob_sales = df_glob_sales.append({"Prix":p,"Achat_Tot":(achat_naiv+achat_strat),"Date":k+1,"Part_Strat":(i/100)},ignore_index=True)
-                    
-                    
     print(df_glob_sales)
     
     df_glob_sales.to_csv("Data_Model_2.csv",index=False)
-
+"""
     
 def load_data(path, train_proportion ,strat_min_prop, step_prop):
+    
     df= pd.read_csv(path)
     df = df.loc[(df["Part_Strat"]>=strat_min_prop) & (df["Part_Strat"]<strat_min_prop+step_prop)]
     df=df.drop(columns=["Part_Strat"])
-    price_grid_temp = df.to_numpy()
-    price_grid_temp=price_grid_temp[:,:].astype(int)
     
-
+    # Convert to numpy
+    price_grid_temp = df.to_numpy()
+    price_grid_temp = price_grid_temp[:,:].astype(int)
+    
+    # Get the train_proportion corresponding to a column index (= train_proportion * columns total)
     proportion = get_proportion(len(price_grid_temp),train_proportion)
-    price_grid_test = price_grid_temp [proportion:len(price_grid_temp),:]
+    
+    # Get the data for test part
+    price_grid_test = price_grid_temp[proportion:len(price_grid_temp),:]
+    
+    # Get the data for train part
     price_grid = price_grid_temp[0:proportion,:]
     
-    return price_grid, price_grid_test ,proportion
-               
-
-
-def testing_data(df_price, df_booked,proportion):
-    # Get test data of 2020
-    data_test = df_price[df_price.columns[proportion:]]
-    data_test_booked = df_booked[df_booked.columns[proportion:]]
-    return data_test, data_test_booked
-
-
-#p,b= testing_data(df_p,df_b,proportion)
-
-#print(p)
-#print(b)
-
-def testing_data_2019(df_price, df_booked):
-    # Get test data of 2019
-    data_test = df_price[df_price.columns[45:57]]
-    data_test_booked = df_booked[df_booked.columns[45:57]]
-    return data_test, data_test_booked
-
-#p,b=testing_data_2019(df_p,df_b)
-
-
-
-def testing_data_2020(df_price, df_booked):
-    # Get test data of 2020
-    data_test = df_price[df_price.columns[57:]]
-    data_test_booked = df_booked[df_booked.columns[57:]]
-    return data_test, data_test_booked
-
-#dt19 , dtb19 = testing_data_2019(df_p, df_b)
-#dt20 , dtb20 =testing_data_2020(df_p, df_b)
-
-
-# create_data_2(12)
-
-# load_data("Data_Model.csv",0.83,0, 0.1)
-    
+    return price_grid, price_grid_test, proportion
